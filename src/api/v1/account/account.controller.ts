19f066ceb44c710fdf79dafd878fec030bcb63a6
Fenish <sohretalhadev@outlook.com>
2023-09-22 02:59:03 +0300

    Change variable names

--- src/api/v1/account/account.controller.ts
+++ src/api/v1/account/account.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AccountService } from './account.service';
-import { Credentials } from '../Database/Dto/create-account';
+import { Credentials } from '../Database/Dto/create.dto';
 
 @Controller('v1/user')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
-  @MessagePattern('Pawn:CreateAccount')
+  @MessagePattern('NB-Auth:CreateAccount')
   async createAccountHandler(data: Credentials) {
     return await this.accountService.createAccount(data);
   }
