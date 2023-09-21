a67f9968086480cc22af47b14a01c079e2742dea
Fenish <sohretalhadev@outlook.com>
2023-09-22 02:59:19 +0300

    Remove microservice

--- src/api/v1/account/account.module.ts
+++ src/api/v1/account/account.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import {
-  BROKERS,
-  AUTHENTICATION_SERVICE_NAME,
-  AUTHENTICATION_QUEUE_NAME,
-} from '@/utils/constants';
+// import { ClientsModule, Transport } from '@nestjs/microservices';
+// import {
+//   BROKERS,
+//   AUTHENTICATION_SERVICE_NAME,
+//   AUTHENTICATION_QUEUE_NAME,
+// } from '@/utils/constants';
 
 // V1
 import { AccountController } from './account.controller';
@@ -13,23 +13,25 @@ import { AccountService } from './account.service';
 
 // Models
 import { Account } from '../Database/Models/account.model';
+import { Permission, Role } from '../Database/Models/role.model';
+import { Database } from '../Database/Models/database.model';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: AUTHENTICATION_SERVICE_NAME,
-        transport: Transport.RMQ,
-        options: {
-          urls: BROKERS,
-          queue: AUTHENTICATION_QUEUE_NAME,
-          queueOptions: {
-            durable: false,
-          },
-        },
-      },
-    ]),
-    SequelizeModule.forFeature([Account]),
+    // ClientsModule.register([
+    //   {
+    //     name: AUTHENTICATION_SERVICE_NAME,
+    //     transport: Transport.RMQ,
+    //     options: {
+    //       urls: BROKERS,
+    //       queue: AUTHENTICATION_QUEUE_NAME,
+    //       queueOptions: {
+    //         durable: false,
+    //       },
+    //     },
+    //   },
+    // ]),
+    SequelizeModule.forFeature([Account, Permission, Role, Database]),
   ],
   controllers: [AccountController],
   providers: [AccountService],
