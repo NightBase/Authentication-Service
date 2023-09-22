6f3dcfd23bc89f7bfb694c57823402b5d89b1a35
Fenish <sohretalhadev@outlook.com>
2023-09-22 03:00:44 +0300

    Create login middleware

---
+++ src/api/v1/middleware/login.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { LoginDto } from '../Database/Dto/login.dto';
+
+@Injectable()
+export class LoginMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const body: LoginDto = req.body;
+    if (!body.identifier || !body.password) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: 'Missing credentials',
+      });
+    }
+    next();
+  }
+}
