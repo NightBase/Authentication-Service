6eaf28e6fec16a0e0fc5a5a5729236945d1f4c2b
Fenish <sohretalhadev@outlook.com>
2023-09-22 03:01:46 +0300

    Remove microservice

--- src/api/v1/token/token.module.ts
+++ src/api/v1/token/token.module.ts
@@ -3,30 +3,9 @@ import { TokenService } from './token.service';
 import { TokenController } from './token.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Account } from '../Database/Models/account.model';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import {
-  ACCOUNT_SERVICE_NAME,
-  BROKERS,
-  ACCOUNT_QUEUE_NAME,
-} from '@/utils/constants';
 
 @Module({
-  imports: [
-    SequelizeModule.forFeature([Account]),
-    ClientsModule.register([
-      {
-        name: ACCOUNT_SERVICE_NAME,
-        transport: Transport.RMQ,
-        options: {
-          urls: BROKERS,
-          queue: ACCOUNT_QUEUE_NAME,
-          queueOptions: {
-            durable: false,
-          },
-        },
-      },
-    ]),
-  ],
+  imports: [SequelizeModule.forFeature([Account])],
   controllers: [TokenController],
   providers: [TokenService],
   exports: [TokenService],
