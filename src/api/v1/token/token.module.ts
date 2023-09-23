ae897c11fe43a6622fc40f211fb7d7639c9f3d94
Fenish <sohretalhadev@outlook.com>
2023-09-23 17:54:10 +0300

    Add global session data

--- src/api/v1/token/token.module.ts
+++ src/api/v1/token/token.module.ts
@@ -4,11 +4,12 @@ import { TokenController } from './token.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Account } from '../Database/Models/account.model';
 import { RefreshToken } from '../Database/Models/token.model';
+import { SessionData } from '@/utils/global';
 
 @Module({
   imports: [SequelizeModule.forFeature([Account, RefreshToken])],
   controllers: [TokenController],
-  providers: [TokenService],
+  providers: [TokenService, SessionData],
   exports: [TokenService],
 })
 export class TokenModule {}
