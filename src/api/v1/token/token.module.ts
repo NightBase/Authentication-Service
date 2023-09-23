196452f3d1804ce852dfe69c7860d095a650c643
Fenish <sohretalhadev@outlook.com>
2023-09-23 03:13:11 +0300

    Implement refreshtoken model

--- src/api/v1/token/token.module.ts
+++ src/api/v1/token/token.module.ts
@@ -3,9 +3,10 @@ import { TokenService } from './token.service';
 import { TokenController } from './token.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Account } from '../Database/Models/account.model';
+import { RefreshToken } from '../Database/Models/token.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Account])],
+  imports: [SequelizeModule.forFeature([Account, RefreshToken])],
   controllers: [TokenController],
   providers: [TokenService],
   exports: [TokenService],
