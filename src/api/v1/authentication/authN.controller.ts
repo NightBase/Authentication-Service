71c959275d7af42114eb5532616d8ef713613160
Fenish <sohretalhadev@outlook.com>
2023-09-23 03:10:58 +0300

    Set cookie and header

--- src/api/v1/authentication/authN.controller.ts
+++ src/api/v1/authentication/authN.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AuthService } from './authN.service';
 import { LoginDto } from '../Database/Dto/login.dto';
@@ -13,7 +13,14 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() data: LoginDto) {
-    return this.authService.login(data);
+  async login(@Body() data: LoginDto, @Res() response: any) {
+    const loginData: any = await this.authService.login(data);
+
+    response.header('Authorization', `Bearer ${loginData.token}`);
+    response.cookie('refreshToken', loginData.refreshToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    return response.status(200).send(loginData.token);
   }
 }
