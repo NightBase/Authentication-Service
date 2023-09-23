d52547153690f8c71677b937a1eff7068a0eb3dc
Fenish <sohretalhadev@outlook.com>
2023-09-23 03:13:55 +0300

    Create token cycle

--- src/api/v1/token/token.service.ts
+++ src/api/v1/token/token.service.ts
@@ -1,29 +1,124 @@
 import { InjectModel } from '@nestjs/sequelize';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Account } from '../Database/Models/account.model';
 import { JwtService } from '@nestjs/jwt';
+import { RefreshToken } from '../Database/Models/token.model';
+
+// TODO: Implement passport.js for session
 
 @Injectable()
 export class TokenService {
   constructor(
-    @InjectModel(Account) private account_model: typeof Account,
+    @InjectModel(Account) private accountModel: typeof Account,
+    @InjectModel(RefreshToken) private refreshTokenModel: typeof RefreshToken,
     private jwtService: JwtService,
   ) {}
 
-  isTokenValid(token: string) {
+  async createToken(account: Account) {
+    const token = this.jwtService.sign({
+      id: account.id,
+      username: account.username,
+    });
+
+    const refreshToken = this.jwtService.sign(
+      {
+        id: account.id,
+        username: account.username,
+      },
+      {
+        expiresIn: '7d',
+      },
+    );
+
+    await this.revokeToken(account.id);
+    await this.refreshTokenModel.create({
+      accountId: account.id,
+      token,
+      refreshToken,
+    });
+
+    return [token, refreshToken];
+  }
+
+  async refreshToken(token: string) {
+    const tokenFromDB = await this.refreshTokenModel.findOne({
+      where: {
+        refreshToken: token,
+      },
+    });
+    if (!tokenFromDB) {
+      return new HttpException(
+        {
+          message: 'Invalid token',
+          logout: true,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const newToken = this.jwtService.sign({
+      id: tokenFromDB.accountId,
+    });
+
+    await this.refreshTokenModel.update(
+      {
+        token: newToken,
+      },
+      {
+        where: {
+          refreshToken: token,
+        },
+      },
+    );
+    return newToken;
+  }
+
+  async isValid(token: string) {
+    const tokenFromDB = await this.refreshTokenModel.findOne({
+      where: {
+        token,
+      },
+    });
+
+    if (!tokenFromDB) {
+      return new HttpException(
+        {
+          message: 'Invalid token',
+          logout: true,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    return true;
+  }
+
+  async isExpired(token: string) {
     try {
       this.jwtService.verify(token);
-      return true;
-    } catch (e) {
       return false;
+    } catch (e) {
+      return true;
     }
   }
 
-  refreshToken(token: string) {
-    return token;
+  isRevoked(token: string) {
+    const tokenFromDB = this.refreshTokenModel.findOne({
+      where: {
+        token,
+      },
+    });
+
+    if (!tokenFromDB) {
+      return true;
+    }
+    return false;
   }
 
-  revokeToken(token: string) {
-    return token;
+  async revokeToken(accountId: string) {
+    return await this.refreshTokenModel.destroy({
+      where: {
+        accountId,
+      },
+    });
   }
 }
