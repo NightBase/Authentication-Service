9a915bd1f715b84f727254144ef4ad7f63f64504
Fenish <sohretalhadev@outlook.com>
2023-09-25 02:59:16 +0300

    Add permission validator

---
+++ src/api/v1/authorization/permission/permission.service.ts
@@ -0,0 +1,23 @@
+import { Account } from '@/api/common/Database/Models/account.model';
+import { SessionData } from '@/utils/global';
+import { Inject } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+export class AuthZPermission {
+  constructor(
+    @Inject(SessionData) private sessionData: SessionData,
+    @InjectModel(Account) private readonly accountModel,
+  ) {}
+
+  async hasRootPermission(token: string) {
+    const session = this.sessionData.getSession(token);
+    const user = await this.accountModel.findOne({
+      where: {
+        username: session.value.username,
+        isRoot: true,
+      },
+    });
+    if (!user) return false;
+    return true;
+  }
+}
