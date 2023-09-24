315cb3576ec3280e00bd541972fa832ccb4974b9
Fenish <sohretalhadev@outlook.com>
2023-09-25 02:58:00 +0300

    Remove unnecessary service

--- src/api/v1/account/account.module.ts
+++ src/api/v1/account/account.module.ts
@@ -1,11 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import {
-  BROKERS,
-  AUTHENTICATION_SERVICE_NAME,
-  AUTHENTICATION_QUEUE_NAME,
-} from '@/utils/constants';
 
 // V1
 import { AccountController } from './account.controller';
@@ -16,22 +10,14 @@ import { Account } from '../../common/Database/Models/account.model';
 import { Permission, Role } from '../../common/Database/Models/role.model';
 import { Database } from '../../common/Database/Models/database.model';
 
+import { TokenModule } from '../token/token.module';
+import { AuthZModule } from '../authorization/authZ.module';
+
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: AUTHENTICATION_SERVICE_NAME,
-        transport: Transport.RMQ,
-        options: {
-          urls: BROKERS,
-          queue: AUTHENTICATION_QUEUE_NAME,
-          queueOptions: {
-            durable: false,
-          },
-        },
-      },
-    ]),
     SequelizeModule.forFeature([Account, Permission, Role, Database]),
+    TokenModule,
+    AuthZModule,
   ],
   controllers: [AccountController],
   providers: [AccountCreateService],
