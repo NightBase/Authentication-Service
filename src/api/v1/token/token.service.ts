1bed336481a24296e7cc2ca0f9e296c45bbcfe81
Fenish <sohretalhadev@outlook.com>
2023-09-27 19:53:56 +0300

    Remove unnecessary errors

--- src/api/v1/token/token.service.ts
+++ src/api/v1/token/token.service.ts
@@ -1,7 +1,8 @@
 import { SessionData } from '@/utils/global';
-import { Account } from '../../common/Database/Models/account.model';
 import { generateRandomString } from '@/utils/stringUtils';
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Account } from '../../common/Database/Models/account.model';
 
 @Injectable()
 export class TokenService {
@@ -22,34 +23,24 @@ export class TokenService {
   async refreshToken(refreshToken: string) {
     const refToken = this.sessionData.findTokenFromRefreshToken(refreshToken);
     if (!refToken) {
-      return new HttpException(
-        {
-          message: 'Invalid token',
-          logout: true,
-        },
-        HttpStatus.UNAUTHORIZED,
-      );
+      return null;
     }
-    this.sessionData.deleteSession(refToken);
     const token = generateRandomString(64);
+    const oldSession = this.sessionData.getSession(refToken);
+
     this.sessionData.setSession(token, {
-      id: refToken.id,
-      username: refToken.username,
+      id: oldSession.value.id,
+      username: oldSession.value.username,
       refreshToken,
     });
+    this.revokeToken(refToken);
     return token;
   }
 
   async isValid(token: string) {
     const session = this.sessionData.getSession(token);
     if (!session) {
-      return new HttpException(
-        {
-          message: 'Token not found in session data',
-          logout: true,
-        },
-        HttpStatus.UNAUTHORIZED,
-      );
+      return false;
     }
     return true;
   }
