6a4b673c473c1a0f828a3ad71d64bfe315932f30
Fenish <sohretalhadev@outlook.com>
2023-09-27 19:53:30 +0300

    Add controller

--- src/api/v1/authorization/authZ.module.ts
+++ src/api/v1/authorization/authZ.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AuthZPermission } from './permission/permission.service';
-import { TokenModule } from '../token/token.module';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { Account } from '@/api/common/Database/Models/account.model';
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { TokenModule } from '../token/token.module';
+import { AuthZController } from './authZ.controller';
+import { AuthZPermission } from './permission/permission.service';
 
 @Module({
   imports: [TokenModule, SequelizeModule.forFeature([Account])],
-  controllers: [],
+  controllers: [AuthZController],
   providers: [AuthZPermission],
   exports: [AuthZPermission],
 })
