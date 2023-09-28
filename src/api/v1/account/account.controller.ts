aa6d5f81a3583dd5546f5b356517dd7e0fb0823a
Fenish <sohretalhadev@outlook.com>
2023-09-28 03:17:45 +0300

    Add delete message

--- src/api/v1/account/account.controller.ts
+++ src/api/v1/account/account.controller.ts
@@ -3,13 +3,25 @@ import { MessagePattern } from '@nestjs/microservices';
 
 import { Credentials } from '../../common/Database/Dto/create.dto';
 import { AccountCreateService } from './create/create.service';
+import { AccountDeleteService } from './delete/delete.service';
 
 @Controller('v1/user')
 export class AccountController {
-  constructor(private readonly accountCreateService: AccountCreateService) {}
+  constructor(
+    private readonly accountCreateService: AccountCreateService,
+    private readonly accountDeleteService: AccountDeleteService,
+  ) {}
 
   @MessagePattern('NB-Auth:CreateAccount')
   async createAccountHandler(data: Credentials) {
     return await this.accountCreateService.createAccount(data);
+  }
+
+  @MessagePattern('NB-Auth:DeleteAccount')
+  async deleteAccountHandler(data: {
+    identifier: string;
+    accessToken: string;
+  }) {
+    return await this.accountDeleteService.deleteAccount(data);
   }
 }
