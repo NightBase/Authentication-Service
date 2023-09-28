6450fc0c1a89edd1210bff5bf63a04ddd2f58bc5
Fenish <sohretalhadev@outlook.com>
2023-09-28 03:18:59 +0300

    Create account get function

---
+++ src/api/v1/account/get/get.service.ts
@@ -0,0 +1,26 @@
+import { Op } from 'sequelize';
+
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import { Account } from '../../../common/Database/Models/account.model';
+import { AuthZPermission } from '../../authorization/permission/permission.service';
+import { TokenService } from '../../token/token.service';
+
+@Injectable()
+export class AccountGetService {
+  constructor(
+    @InjectModel(Account) private readonly accountModel,
+    private readonly tokenService: TokenService,
+    private readonly authZPerm: AuthZPermission,
+  ) {}
+
+  async getAccount(identifier: string) {
+    const account = await this.accountModel.findOne({
+      where: {
+        [Op.or]: [{ username: identifier }, { email: identifier }],
+      },
+    });
+    return !!account;
+  }
+}
